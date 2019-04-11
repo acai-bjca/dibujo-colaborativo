591b1389736152589adf02472fdf86cd5846faa3
acai-bjca <amalia.alfonso@mail.escuelaing.edu.co>
2019-04-11 18:56:16 -0500

    actualizando servidor de broker de mensajes a ActiveMQ
    Co-authored-by: CarlosCL98 <camer.cl98@gmail.com>

--- src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/collabpaint/CollabPaintWebSocketConfig.java
@@ -34,13 +34,15 @@ public class CollabPaintWebSocketConfig extends AbstractWebSocketMessageBrokerCo
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");        
+        //config.enableSimpleBroker("/topic");
+        config.enableStompBrokerRelay("/topic/").setRelayHost("192.168.56.101").setRelayPort(61613);
+        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/stompendpoint").withSockJS();
+        //registry.addEndpoint("/stompendpoint").withSockJS();
+        registry.addEndpoint("/stompendpoint").setAllowedOrigins("*").withSockJS();
         
     }
     
